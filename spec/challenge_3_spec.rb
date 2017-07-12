04eceec62b87443bd14627ce1e9e9d7389904e82
guidohcosta <guidohcosta@gmail.com>
2017-07-12 16:54:08 -0300

    Desafio 3

---
+++ spec/challenge_3_spec.rb
@@ -0,0 +1,29 @@
+require './lib/challenge_3'
+
+describe DevMagic do
+  subject { DevMagic.print(number) }
+
+  context "regular number" do
+    let(:number) { [ 1, 2, 4, 7 ].sample }
+
+    it "prints the number" do
+      is_expected.to eq number.to_s
+    end
+  end
+
+  context "divisible by 3" do
+    let(:number) { [ 3, 6, 9, 12 ].sample }
+
+    it "prints dev" do
+      is_expected.to eq "Dev"
+    end
+  end
+
+  context "divisible by 5" do
+    let(:number) { [ 5, 10, 15, 20 ].sample }
+
+    it "prints dev" do
+      is_expected.to eq "Magic"
+    end
+  end
+end
