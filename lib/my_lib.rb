e45cdf02226b741db9d7ffc3e6d0b493d13c95cd
henriquefernandez <hriqueft@gmail.com>
2017-07-18 20:26:19 -0300

    desafio 4

--- lib/my_lib.rb
+++ lib/my_lib.rb
@@ -1,5 +1,13 @@
 arguments = ARGV
 
+def desafio1
+	"true. 0 && !nil é igual a true e o && tem precedência maior que and. Logo o resultado da operação se torna true and true and true retornando true."
+end
+
+def desafio2
+	"O operador ||= verifica se a variável ja foi preenchida, caso não foi, ou a variável seja nil, atribui o valor."
+end
+
 def desafio3
 
 	(1..100).each do |number|
@@ -18,20 +26,11 @@ def desafio3
 end
 
 def desafio4(sentenca)
-
-	(1..100).each do |number|
-
-		if number % 3 == 0 && number % 5 == 0
-			p "#{number}:_DevMagic_"
-		elsif number % 3 == 0
-			p "#{number}:_Dev_"
-		elsif number % 5 == 0
-			p "#{number}:_Magic_"
-		else
-			p "#{number}"
-		end
+	if sentenca.respond_to?(:to_str)
+		(sentenca.downcase.split == sentenca.downcase.split.reverse.map {|word| word.reverse })? 'É um palíndromo' : 'Não é um palindroma' 
+	else
+		"O argumento deve ser uma string e deve estar entre '' "
 	end
-
 end
 
 def home
@@ -39,11 +38,11 @@ def home
     p '-----------------------------------------------------------------------------------'
     p '| .+* . * + . * +* . * + . * . Desafio DevMagic + + . + * .+ *. *.+ + . * .* . *  |'
     p '-----------------------------------------------------------------------------------'
-    p '# ruby my_lib.rb desafio1                  Responde o desafio 1'   
-    p '# ruby my_lib.rb desafio2                  Responde o desafio 2'  
-    p '# ruby my_lib.rb desafio3                  Executa o desafio 3'   
-    p '# ruby my_lib.rb desafio4 [arg]            Executa o desafio 4'  
-    p '# ruby my_lib.rb desafio5 [arg]            Executa o desafio 5'   
+    p '# ruby my_lib.rb desafio1                    Responde o desafio 1'   
+    p '# ruby my_lib.rb desafio2                    Responde o desafio 2'  
+    p '# ruby my_lib.rb desafio3                    Executa o desafio 3'   
+    p '# ruby my_lib.rb desafio4 ["arg"]            Executa o desafio 4'  
+    p '# ruby my_lib.rb desafio5 ["arg"]            Executa o desafio 5'   
     p '-----------------------------------------------------------------------------------'
     
 end
@@ -54,30 +53,29 @@ case arguments.shift
 	    p '-----------------------------------------------------------------------------------'
 	    p '| .+* . * + . * +* . * + . * . Desafio DevMagic + + . + * .+ *. *.+ + . * .* . *  |'
 	    p '-----------------------------------------------------------------------------------'   
-    	p " Desafio 1: true. 0 && !nil é igual a true e o && tem precedência maior que and. Logo o resultado da operação se torna true and true and true retornando true."
+    	p " Desafio 1: #{desafio1}"
       p '-----------------------------------------------------------------------------------'
 
     when 'desafio2'
 	    p '-----------------------------------------------------------------------------------'
 	    p '| .+* . * + . * +* . * + . * . Desafio DevMagic + + . + * .+ *. *.+ + . * .* . *  |'
 	    p '-----------------------------------------------------------------------------------'   
-    	p " Desafio 2: O operador ||= verifica se a variável ja foi preenchida, caso não foi, ou a variável seja nil, atribui o valor."
+    	p " Desafio 2: #{desafio2}"
         p '-----------------------------------------------------------------------------------'
 
     when 'desafio3'
 	    p '-----------------------------------------------------------------------------------'
 	    p '| .+* . * + . * +* . * + . * . Desafio DevMagic + + . + * .+ *. *.+ + . * .* . *  |'
 	    p '-----------------------------------------------------------------------------------'   
+	    p " Desafio 3:"
     	desafio3
 
     when 'desafio4'
 
-    	p arguments.inspect
-
 	    p '-----------------------------------------------------------------------------------'
 	    p '| .+* . * + . * +* . * + . * . Desafio DevMagic + + . + * .+ *. *.+ + . * .* . *  |'
-	    p '-----------------------------------------------------------------------------------'   
-    	desafio4
+	    p '-----------------------------------------------------------------------------------'    
+	    p " Desafio 4: #{desafio4 arguments.shift}"  
 
     else
         home
