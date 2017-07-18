68d4780562f5387b5ac638638529c5b9bd941b02
henriquefernandez <hriqueft@gmail.com>
2017-07-18 19:40:51 -0300

    menu interativo

---
+++ lib/my_lib.rb
@@ -0,0 +1,37 @@
+arguments = ARGV
+
+def home
+    
+    p '-----------------------------------------------------------------------------------'
+    p '| .+* . * + . * +* . * + . * . Desafio DevMagic + + . + * .+ *. *.+ + . * .* . *  |'
+    p '-----------------------------------------------------------------------------------'
+    p '# ruby my_lib.rb desafio1          Responde o desafio 1'   
+    p '# ruby my_lib.rb desafio2          Responde o desafio 2'  
+    p '# ruby my_lib.rb desafio3          Executa o desafio 3'   
+    p '# ruby my_lib.rb desafio4          Executa o desafio 4'  
+    p '# ruby my_lib.rb desafio5          Executa o desafio 5'   
+    p '-----------------------------------------------------------------------------------'
+    
+end
+
+case arguments.shift
+
+    when 'desafio1'
+	    p '-----------------------------------------------------------------------------------'
+	    p '| .+* . * + . * +* . * + . * . Desafio DevMagic + + . + * .+ *. *.+ + . * .* . *  |'
+	    p '-----------------------------------------------------------------------------------'   
+    	p " Desafio 1: true. 0 && !nil é igual a true e o && tem precedência maior que and. Logo o resultado da operação se torna true and true and true retornando true."
+        p '-----------------------------------------------------------------------------------'
+
+    when 'desafio2'
+	    p '-----------------------------------------------------------------------------------'
+	    p '| .+* . * + . * +* . * + . * . Desafio DevMagic + + . + * .+ *. *.+ + . * .* . *  |'
+	    p '-----------------------------------------------------------------------------------'   
+    	p " Desafio 2: O operador ||= verifica se a variável ja foi preenchida, caso não foi, ou a variável seja nil, atribui o valor."
+        p '-----------------------------------------------------------------------------------'
+      
+    else
+        
+        home
+    
+end
